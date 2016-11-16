3b6889d475d8bd5ccf3617d7b8b0b44b3d37bef7
JureStabuc <jure.stabuc@gmail.com>
2016-11-16 18:37:25 +0000

    sorting scores

--- jumble_game.py
+++ jumble_game.py
@@ -1,5 +1,6 @@
 import random
 import shelve
+import time
 
 # Welcome the player
 print("""
@@ -7,6 +8,8 @@ print("""
         Unscramble the letters to make a word.
 """)
 
+
+#Main menu
 def menu():
     print("1--Play the game")
     print("2--Browse a word set")
@@ -14,8 +17,9 @@ def menu():
     print("4--Delete a word set")
     print("5--My sorted scores")
     print("6--Exit")
-    pick = int(input("Pick one:"))
+    pick = int(input("Please select: "))
 
+#When user picks a number the assigned function runs
     if pick == 1:
         game()
 
@@ -34,62 +38,85 @@ def menu():
     elif pick == 5:
         score()
 
+#Browse a wordlist function
 def browse():
     print("Retrieving word list from shelf")
+    #Open the wordlist
     shelf = shelve.open("wordlists.dat")
+    #Putting the keys into a list
     listname = list(shelf.keys())
+    #Making the menu with keys and the assigned index number
     for i in listname:
         print("{}--{}".format(listname.index(i)+1,i))
-    choice = int(input("Pick one:"))
+    choice = int(input("Please select: "))
+    #Chosing the correct key from user input according to the key's index
     word_set=(listname[choice-1])
-    print("Your words: {}".format(shelf[word_set]))
+    print("Your words:")
+    for i in shelf[word_set]:
+        print("{}".format(i))
     shelf.close()
     return menu()
+
+    #the_list = [word.strip(",") for line in afile for word in line.split()]
 
 def game():
     shelf=shelve.open("wordlists.dat")
     listname = list(shelf.keys())
     for i in listname:
         print("{}--{}".format(listname.index(i)+1,i))
-    choice = int(input("Pick one:"))
+    choice = int(input("Please select: "))
     word_set=(listname[choice-1])
     global wordlist
     wordlist = shelf[word_set]
     score = 0
+    #Choosing a random word from the wordlist
     for i in range(4):
         word = random.choice(wordlist)
         theWord = word
         jumble = ""
+        #Jumbling the word
         while(len(word) > 0):
             position = random.randrange(len(word))
             jumble += word[position]
             word = word[:position] + word[position + 1:]
         print("The jumble word is: {}".format(jumble))
-
         # Getting player's guess
         guess = input("Enter your guess: ").lower()
-
-        # congratulate the player
+        # Congratulate the player
         if(guess == theWord):
             print("Congratulations! You guessed it")
             score += 1
-
         else:
-            print("Sorry, wrong guess.")
+            print("Sorry, wrong guess. The word is {}".format(theWord))
+    #Printing the score
     print("You got {} out of 10".format(score))
+    #Recording the score
     shelf = shelve.open("score.dat")
-    shelf["score"]=[score]
+    score_a = str(score)
+    shelf[score_a]=[time.ctime()]
     shelf.sync()
     shelf.close()
     return menu()
-
+#Displaying user's scores
 def score():
+    myformat = "{:10s} {:13s}"
+    print(myformat.format('Score', 'Date'))
+    print("-"*26)
     shelf = shelve.open("score.dat")
-    for key in shelf.keys():
-        print(shelf[key])
-    shelf.close()
+    listname = list(shelf.keys())
+    for num in range(len(listname)-1,0,-1):
+        for i in range(num):
+            if listname[i]<listname[i+1]:
+                temp = listname[i+1]
+                listname[i+1] = listname[i]
+                listname[i] = temp
+    for key in listname:
+        dates = shelf[key]
+        for val in dates:
+            print(myformat.format(key, val))
+    shelf.close
     return menu()
-
+#Adding a wordset
 def add_list(file):
     with open(file) as afile:
         the_list = [word.strip(",") for line in afile for word in line.split()]
@@ -106,7 +133,7 @@ def delete():
     listname = list(shelf.keys())
     for i in listname:
         print("{}--{}".format(listname.index(i)+1,i))
-    choice = int(input("Pick one:"))
+    choice = int(input("Please select: "))
     word_set=(listname[choice-1])
     #if word_set == i in listname.keys():
     del shelf[word_set]
--- practice.py
+++
@@ -1,7 +0,0 @@
-import shelve
-shelf=shelve.open("wordlists.dat")
-listname = list(shelf.keys())
-for i in listname:
-    print("{}--{}".format(listname.index(i),i))
-word_set = int(input("Pick one:"))
-print(listname[word_set])
